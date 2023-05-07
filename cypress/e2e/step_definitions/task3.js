ad4d0b9394e215ab833bff29ce12135e59be660f
tomfriday <31045046+tomfriday@users.noreply.github.com>
2023-05-07 20:42:08 +0200

    test

--- cypress/e2e/step_definitions/task3.js
+++ cypress/e2e/step_definitions/task3.js
@@ -1,7 +1,7 @@
 import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps'
 import { newPage } from '../../pages/NewPage'
 import { wskzMainPage } from '../../pages/WskzMainPage'
-import studentInfo from '../../fixtures/StudentInfo.json'
+import studentInfo from '../../fixtures/studentInfo.json'
 
 Given('Visit new-page', () => {
   cy.visit('/' + '/new-page')
